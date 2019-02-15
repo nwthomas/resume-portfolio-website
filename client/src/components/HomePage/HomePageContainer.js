2baa8920d318e4537a22caa6e440520003931584
Nathan Thomas <nwthomas@me.com>
2019-02-15 13:34:08 -0800

    Update with useEffect in App.js

--- client/src/components/HomePage/HomePageContainer.js
+++ client/src/components/HomePage/HomePageContainer.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HeaderContainer } from "../HeaderContent";
 import { FeaturedWorkContainer } from "../FeaturedWork";
 import { Footer } from "../Footer";
 import PropTypes from "prop-types";
 
 const HomePageContainer = props => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <HeaderContainer darkmode={props.darkmode} />
