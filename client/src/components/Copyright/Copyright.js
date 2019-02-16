57a4d194d69918e181f5dd5e2f4f4471c56085c8
Nathan Thomas <nwthomas@me.com>
2019-02-15 19:45:46 -0800

    Update README with new screenshot and Trello board

---
+++ client/src/components/Copyright/Copyright.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Copyright = props => {
+  return (
+    <div>
+      <div />
+    </div>
+  );
+};
+
+Copyright.propTypes = {};
+
+export default Copyright;
