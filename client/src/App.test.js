772e17d747f49589c9fc51f034d753344a27d704
Nathan Thomas <nwthomas@me.com>
2019-03-06 21:34:24 -0800

    Wire up App.js tests including testHelpers for router

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+import { renderWithRouter, rendererWithRouter } from "./testHelpers"; // Necessary to simulate React Router
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+describe("<App />", () => {
+  it("renders the App component without crashing", () => {
+    const push = jest.fn();
+    const scrollToSpy = jest.fn();
+    window.scrollTo = scrollToSpy;
+    const helpers = renderWithRouter(<App history={push} />);
+  });
+
+  it("matches the snapshot of App", () => {
+    const tree = rendererWithRouter(<App />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
 });
