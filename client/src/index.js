a137ab243bcb06d9805a24413bd8daed551b49d5
Nathan Thomas <nwthomas@me.com>
2019-07-31 19:05:27 -0700

    Add Sentry error tracking to site

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 import App from "./App";
 import { BrowserRouter as Router, withRouter } from "react-router-dom";
 import { StateProvider } from "react-conflux";
 import { globalReducer } from "./store/reducers/globalReducer";
 import { darkContext } from "./store/contexts";
+require("dotenv").config();
+
+Sentry.init({
+  dsn: process.env.REACT_SENTRY_VAR
+});
 
 const AppWithRouter = withRouter(App);
 
@@ -16,8 +22,3 @@ ReactDOM.render(
   </StateProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
